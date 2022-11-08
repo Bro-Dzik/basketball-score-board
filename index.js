06950a32f57dde9bbe2f5192293fd92a9383d481
Bro-Dzik <canabis1983@tlen.pl>
2022-11-08 10:20:41 +0100

    meta tag and javascript refactored

--- index.js
+++ index.js
@@ -2,37 +2,54 @@ let homePoints = 0
 let guestPoints = 0
 let hpEl = document.getElementById("hp-el")
 let gpEl = document.getElementById("gp-el")
+const onehBtn = document.getElementById("oneh-btn")
+const twohBtn = document.getElementById("twoh-btn")
+const threehBtn = document.getElementById("threeh-btn")
+const onegBtn = document.getElementById("oneg-btn")
+const twogBtn = document.getElementById("twog-btn")
+const threegBtn = document.getElementById("threeg-btn")
+const newEl = document.getElementById("new")
 
-function homeone() {
+  onehBtn.addEventListener("click",function () {
     homePoints += 1
     hpEl.textContent = homePoints
     
-}
-function hometwo() {
+}) 
+
+twohBtn.addEventListener("click",function () {
     homePoints += 2
     hpEl.textContent = homePoints
-}
-function homethree(){
+})
+ 
+threehBtn.addEventListener("click",function (){
     homePoints += 3
     hpEl.textContent = homePoints
-}
+})
 
-function guestone(){
+onegBtn.addEventListener("click",function(){
     guestPoints += 1
     gpEl.textContent = guestPoints
-}
-function guesttwo(){
+})
+
+twogBtn.addEventListener("click",function (){
     guestPoints += 2
     gpEl.textContent = guestPoints
-}
-function guestthree(){
+})
+
+threegBtn.addEventListener("click",function (){
     guestPoints += 3
     gpEl.textContent = guestPoints
-}
+})
 
-function newgame(){
+newEl.addEventListener("click",function(){
     hpEl.textContent = 0
     gpEl.textContent = 0
     homePoints = 0
     guestPoints = 0 
-}
+})
+
+
+
+
+
+
